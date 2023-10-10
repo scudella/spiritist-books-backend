997392ff9bd6db88097e79a8749bd2f22aaa4123
Eduardo S. Libardi <eslibardi@gmail.com>
2023-10-10 10:52:42 -0300

    Error Page

--- react-client/src/pages/Error.jsx
+++ react-client/src/pages/Error.jsx
@@ -1,13 +1,57 @@
 import { Link, useRouteError } from 'react-router-dom';
+import styled from 'styled-components';
+import img from '../assets/images/not-found.svg';
+
 const Error = () => {
   const error = useRouteError();
-  console.log(error);
+
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt='não encontrada' />
+          <h3>Página não encontrada!</h3>
+          <p>parece que não encontramos a página que você está procurando</p>
+          <Link to='./dashboard'>página inicial</Link>
+        </div>
+      </Wrapper>
+    );
+  }
   return (
-    <div>
-      <h1>Error Page</h1>
-      <Link to='/'>back home</Link>
-    </div>
+    <Wrapper>
+      <div>
+        <h3>alguma coisa saiu errada</h3>
+      </div>
+    </Wrapper>
   );
 };
 
 export default Error;
+
+const Wrapper = styled.main`
+  min-height: 100vh;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  img {
+    width: 90vw;
+    max-width: 600px;
+    display: block;
+    margin-bottom: 2rem;
+    margin-top: -3rem;
+  }
+  h3 {
+    margin-bottom: 0.5rem;
+  }
+  p {
+    line-height: 1.5;
+    margin-top: 0.5rem;
+    margin-bottom: 1rem;
+    color: var(--text-secondary-color);
+  }
+  a {
+    color: var(--primary-500);
+    text-transform: capitalize;
+  }
+`;
