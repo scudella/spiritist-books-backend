f0012fb45036836eb7ec2affe2f948aea69b8c41
Eduardo S. Libardi <eslibardi@gmail.com>
2023-10-11 17:14:17 -0300

    Dark Theme

---
+++ react-client/src/components/ThemeToggle.jsx
@@ -0,0 +1,32 @@
+import { BsFillSunFill, BsFillMoonFill } from 'react-icons/bs';
+import styled from 'styled-components';
+import { useDashboardContext } from '../pages/DashboardLayout';
+
+const ThemeToggle = () => {
+  const { isDarkTheme, toggleDarkTheme } = useDashboardContext();
+
+  return (
+    <Wrapper onClick={toggleDarkTheme}>
+      {isDarkTheme ? (
+        <BsFillSunFill className='toggle-icon' />
+      ) : (
+        <BsFillMoonFill />
+      )}
+    </Wrapper>
+  );
+};
+export default ThemeToggle;
+
+const Wrapper = styled.button`
+  background: transparent;
+  border-color: transparent;
+  width: 3.5rem;
+  height: 2rem;
+  display: grid;
+  place-items: center;
+  cursor: pointer;
+  .toggle-icon {
+    font-size: 1.15rem;
+    color: var(--text-color);
+  }
+`;
