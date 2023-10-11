bcbd11c85cb7624ee1ab6554188a740b59a09043
Eduardo S. Libardi <eslibardi@gmail.com>
2023-10-11 10:00:27 -0300

    initial state setup for sidebar and dark theme

--- react-client/src/components/SmallSidebar.jsx
+++ react-client/src/components/SmallSidebar.jsx
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
+import { useDashboardContext } from '../pages/DashboardLayout';
 
 const SmallSidebar = () => {
+  const data = useDashboardContext();
+  console.log(data);
+
   return <Wrapper>SmallSidebar</Wrapper>;
 };
 
